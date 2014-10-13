92cec7bad688251357318ff08d809ba6d3a02cd9
JohnLui <wenhanlv@gmail.com>
2014-10-13 16:00:34 +0800

    引入 Macaw 路由

---
+++ config/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use NoahBuscher\Macaw\Macaw;
+
+Macaw::get('fuck', function() {
+  echo "成功！";
+});
+
+Macaw::get('(:all)', function($fu) {
+  echo '未匹配到路由<br>'.$fu;
+});
+
+Macaw::dispatch();
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+// Autoload 自动载入
+require '../vendor/autoload.php';
+
+// 路由配置
+require '../config/routes.php';
