bda1082985d9c9b624668887615ab72d3e916b4c
JohnLui <wenhanlv@gmail.com>
2014-10-16 02:35:38 +0800

    使用ORM 完成

--- app/models/Article.php
+++ app/models/Article.php
@@ -2,25 +2,7 @@
 /**
 * Article Model
 */
-class Article
+class Article extends Illuminate\Database\Eloquent\Model
 {
-  public static function first()
-  {
-    $connection = mysql_connect("localhost","root","C4F075C4");
-    if (!$connection) {
-      die('Could not connect: ' . mysql_error());
-    }
-
-    mysql_set_charset("UTF8", $connection);
-
-    mysql_select_db("mffc", $connection);
-
-    $result = mysql_query("SELECT * FROM articles limit 0,1");
-
-    if ($row = mysql_fetch_array($result)) {
-      return $row;
-    }
-
-    mysql_close($connection);
-  }
+  public $timestamps = false;
 }
--- public/index.php
+++ public/index.php
@@ -1,7 +1,13 @@
 <?php
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 // Autoload 自动载入
 require '../vendor/autoload.php';
+
+// Eloquent ORM
+$capsule = new Capsule;
+$capsule->addConnection(require '../config/database.php');
+$capsule->bootEloquent();
 
 // 路由配置
 require '../config/routes.php';
